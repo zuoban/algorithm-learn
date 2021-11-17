a2320e8124ba2af76ba5e7b96b5fc84ade906c11
wangjq <1061669148@qq.com>
2021-11-17 16:16:06 +0800

    插入排序-优化

--- src/main/java/selectionsort/InsertionSort.java
+++ src/main/java/selectionsort/InsertionSort.java
@@ -5,15 +5,19 @@ import support.Sortable;
 import util.SortingHelper;
 
 public class InsertionSort<E extends Comparable<E>> implements Sortable<E> {
+    public static void main(String[] args) {
+        int[] dataSize = {10000, 100000};
+        for (int n : dataSize) {
+            Integer[] arr = ArrayGenerator.generateRandomArray(n, n);
+            SortingHelper.sortTest(new InsertionSort<>(), arr);
+        }
+    }
+
     @Override
     public void sort(E[] arr) {
         for (int i = 0; i < arr.length; i++) {
-            for (int j = i; j > 0; j--) {
-                if (arr[j].compareTo(arr[j - 1]) < 0) {
-                    swap(arr, j, j - 1);
-                } else {
-                    break;
-                }
+            for (int j = i; j > 0 && arr[j].compareTo(arr[j - 1]) < 0; j--) {
+                swap(arr, j, j - 1);
             }
         }
 
@@ -23,13 +27,5 @@ public class InsertionSort<E extends Comparable<E>> implements Sortable<E> {
         E tmp = arr[i];
         arr[i] = arr[j];
         arr[j] = tmp;
-    }
-
-    public static void main(String[] args) {
-        int[] dataSize = {10000, 100000};
-        for (int n : dataSize) {
-            Integer[] arr = ArrayGenerator.generateRandomArray(n, n);
-            SortingHelper.sortTest(new InsertionSort<>(), arr);
-        }
     }
 }
