a5cb218b1f05d0e039f6f4cbfaef370ab3fceb17
wangjq <1061669148@qq.com>
2021-11-17 21:29:49 +0800

    数组-优化缩容条件 1/4 时， 缩小为 1/2

--- src/main/java/array/Array.java
+++ src/main/java/array/Array.java
@@ -53,12 +53,12 @@ public class Array<E> {
         for (int i = index; i < size; i++) {
             data[i] = data[i + 1];
         }
+        size--;
         // help gc
         data[size] = null;
-        if (size == data.length / 2) {
+        if (size == data.length / 4 && data.length / 2 != 0) {
             resize(data.length / 2);
         }
-        size--;
         return ret;
     }
 
