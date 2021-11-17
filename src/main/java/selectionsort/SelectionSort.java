927755c8ba5f529614d8ba4e0ae5a2793a5ae6fc
wangjq <1061669148@qq.com>
2021-11-17 14:29:07 +0800

    选择排序-添加泛型支持

--- src/main/java/selectionsort/SelectionSort.java
+++ src/main/java/selectionsort/SelectionSort.java
@@ -6,12 +6,12 @@ public class SelectionSort {
     private SelectionSort() {
     }
 
-    public static void sort(int[] arr) {
+    public static <E extends Comparable<E>> void sort(E[] arr) {
         for (int i = 0; i < arr.length; i++) {
             // 选择 arr[i...n) 中的最小值的索引
             int minIndex = i;
             for (int j = i + 1; j < arr.length; j++) {
-                if (arr[j] < arr[minIndex]) {
+                if (arr[j].compareTo(arr[minIndex]) < 0) {
                     minIndex = j;
                 }
             }
@@ -19,14 +19,14 @@ public class SelectionSort {
         }
     }
 
-    private static void swap(int[] arr, int i, int j) {
-        int tmp = arr[i];
+    private static <E> void swap(E[] arr, int i, int j) {
+        E tmp = arr[i];
         arr[i] = arr[j];
         arr[j] = tmp;
     }
 
     public static void main(String[] args) {
-        int[] arr = {1, 4, 2, 3, 6, 5};
+        Integer[] arr = {1, 4, 2, 3, 6, 5};
         sort(arr);
         System.out.println(Arrays.toString(arr));
     }
