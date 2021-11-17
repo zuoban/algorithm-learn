c8287c955fabdce5a5732ee4225fd49b3524f27f
wangjq <1061669148@qq.com>
2021-11-17 16:34:01 +0800

    插入排序-优化，不交换元素

--- src/main/java/selectionsort/InsertionSort.java
+++ src/main/java/selectionsort/InsertionSort.java
@@ -13,14 +13,31 @@ public class InsertionSort<E extends Comparable<E>> implements Sortable<E> {
         }
     }
 
-    @Override
-    public void sort(E[] arr) {
+    /**
+     * 优化前
+     */
+    public void sort2(E[] arr) {
         for (int i = 0; i < arr.length; i++) {
             for (int j = i; j > 0 && arr[j].compareTo(arr[j - 1]) < 0; j--) {
                 swap(arr, j, j - 1);
             }
         }
 
+    }
+
+    /**
+     * 优化后
+     */
+    @Override
+    public void sort(E[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            E t = arr[i];
+            int j;
+            for (j = i; j - 1 >= 0 && t.compareTo(arr[j - 1]) < 0; j--) {
+                arr[j] = arr[j - 1];
+            }
+            arr[j] = t;
+        }
     }
 
     private void swap(E[] arr, int i, int j) {
