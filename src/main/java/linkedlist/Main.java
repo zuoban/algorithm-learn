7dcc12c6303e3792bc760e1f1dd4f28f680b35d3
wangjq <1061669148@qq.com>
2021-11-21 15:27:50 +0800

    链表-使用链表实现栈

--- src/main/java/linkedlist/Main.java
+++ src/main/java/linkedlist/Main.java
@@ -1,23 +1,38 @@
 package linkedlist;
 
+
+import stack.ArrayStack;
+import stack.Stack;
+
+import java.util.Random;
+
 public class Main {
-    public static void main(String[] args) {
-        LinkedList<Integer> linkedList = new LinkedList<>();
-        for (int i = 0; i < 5; i++) {
-            linkedList.addFirst(i);
-            System.out.println(linkedList);
+    private static double testStack(Stack<Integer> stack, int opCount) {
+        long startTime = System.nanoTime();
+        Random random = new Random();
+
+        for (int i = 0; i < opCount; i++) {
+            stack.push(random.nextInt(Integer.MAX_VALUE));
         }
 
-        linkedList.add(2, 666);
-        System.out.println(linkedList);
+        for (int i = 0; i < opCount; i++) {
+            stack.pop();
+        }
 
-        linkedList.remove(2);
-        System.out.println(linkedList);
+        long endTime = System.nanoTime();
 
-        linkedList.removeFirst();
-        System.out.println(linkedList);
+        return (endTime - startTime) / 1000000000.0;
+    }
 
-        linkedList.removeLast();
-        System.out.println(linkedList);
+    public static void main(String[] args) {
+        int opCount = 1000000;
+        ArrayStack<Integer> arrayStack = new ArrayStack<>();
+        double time1 = testStack(arrayStack, opCount);
+        System.out.println("ArrayStack, time: " + time1 + " s");
+
+        LinkedListStack<Integer> linkedListStack = new LinkedListStack<>();
+        double time2 = testStack(linkedListStack, opCount);
+        System.out.println("LinkedListStack, time: " + time2 + " s");
+
     }
 }
