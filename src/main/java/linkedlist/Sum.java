36d0de8fce448b7317daaaa34eb310673d49bb60
wangjq <1061669148@qq.com>
2021-11-21 17:01:31 +0800

    链表-递归-sum

---
+++ src/main/java/linkedlist/Sum.java
@@ -0,0 +1,23 @@
+package linkedlist;
+
+public class Sum {
+
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5, 6, 7, 8};
+        System.out.println(sum(nums));
+
+    }
+
+    public static int sum(int[] arr) {
+        return sum(arr, 0);
+    }
+
+    private static int sum(int[] arr, int l) {
+        if (l == arr.length) {
+            return 0;
+        }
+        return arr[l] + sum(arr, l + 1);
+    }
+
+}
