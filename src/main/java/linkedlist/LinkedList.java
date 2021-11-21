40d0db9ed63c55ea12577725e22b9ddb3b8f3b9d
wangjq <1061669148@qq.com>
2021-11-21 15:16:30 +0800

    链表-实现

---
+++ src/main/java/linkedlist/LinkedList.java
@@ -0,0 +1,168 @@
+package linkedlist;
+
+public class LinkedList<E> {
+    int size;
+    private Node head;
+    private Node dummyHead;
+
+    public LinkedList() {
+        dummyHead = new Node(null, null);
+    }
+
+    /**
+     * 获取链表第 index(0-based) 个位置的元素
+     * 在链表中不是一个常用操作
+     */
+    public E get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Get failed. Illegal index.");
+        }
+
+        Node cur = dummyHead.next;
+        for (int i = 0; i < index; i++) {
+            cur = cur.next;
+        }
+        return cur.e;
+    }
+
+    public E getFirst() {
+        return get(0);
+    }
+
+    public E getLast() {
+        return get(size - 1);
+    }
+
+    public void set(int index, E e) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Update failed. Illegal index.");
+        }
+
+        Node cur = dummyHead.next;
+        for (int i = 0; i < index; i++) {
+            cur = cur.next;
+        }
+        cur.e = e;
+    }
+
+    public boolean contains(E e) {
+        Node cur = dummyHead.next;
+        while (cur != null) {
+            if (cur.e.equals(e)) {
+                return true;
+            }
+            cur = cur.next;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        Node cur = dummyHead.next;
+        while (cur != null) {
+            res.append(cur).append("->");
+            cur = cur.next;
+        }
+        res.append("NULL");
+        return res.toString();
+    }
+
+    /**
+     * 获取链表中的元素个数
+     */
+    public int getSize() {
+        return size;
+    }
+
+    /**
+     * 是否为空
+     */
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    /**
+     * 在链表头添加 e
+     */
+    public void addFirst(E e) {
+        add(0, e);
+    }
+
+    /**
+     * 从链表中删除 index( 0 - based) 位置的元素，返回删除的元素
+     */
+    public E remove(int index) {
+        if (size < 0 || index > size) {
+            throw new IllegalArgumentException("Remove failed. Illegal index.");
+        }
+        Node prev = dummyHead;
+        for (int i = 0; i < index; i++) {
+            prev = prev.next;
+        }
+
+        Node delNode = prev.next;
+        prev.next = delNode.next;
+        delNode.next = null;
+        size--;
+        return delNode.e;
+    }
+
+    public E removeFirst() {
+        return remove(0);
+    }
+
+    public E removeLast() {
+        return remove(size - 1);
+    }
+
+    /**
+     * 在链表的 index (0-based)位置添加新的元素 e
+     * 在链表中不是一个常用的操作，练习用
+     */
+    public void add(int index, E e) {
+        if (size < 0 || index > size) {
+            throw new IllegalArgumentException("Add failed. Illegal index.");
+        }
+
+        Node prev = dummyHead;
+        for (int i = 0; i < index; i++) {
+            prev = prev.next;
+        }
+
+        prev.next = new Node(e, prev.next);
+        size++;
+    }
+
+    /**
+     * 在链表末尾添加 e
+     */
+    public void addLast(E e) {
+        add(size, e);
+    }
+
+    private class Node {
+        public E e;
+        public Node next;
+
+        public Node(E e, Node next) {
+            this.e = e;
+            this.next = next;
+        }
+
+        public Node(E e) {
+            this(e, null);
+        }
+
+        public Node() {
+            this(null, null);
+        }
+
+        @Override
+        public String toString() {
+            return e.toString();
+        }
+    }
+
+
+}
