b691b73d63004e1e50382942d4275e5e3752d0c7
wangjq <1061669148@qq.com>
2021-11-22 20:34:38 +0800

    快速排序-双路快排

--- src/main/java/quicksort/QuickSort.java
+++ src/main/java/quicksort/QuickSort.java
@@ -1,11 +1,9 @@
 package quicksort;
 
 import linearsearch.ArrayGenerator;
-import mergesort.MergeSort;
 import support.Sortable;
 import util.SortingHelper;
 
-import java.util.Arrays;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class QuickSort<E extends Comparable<E>> implements Sortable<E> {
@@ -24,7 +22,37 @@ public class QuickSort<E extends Comparable<E>> implements Sortable<E> {
         sort(arr, p + 1, r);
     }
 
+    /**
+     * 双路快速排序
+     */
     private int partition(E[] arr, int l, int r) {
+        int p = l + ThreadLocalRandom.current().nextInt(r-l+1);
+        swap(arr, l,p);
+
+        // arr[l+1 ... i-1] <= v ; arr[j+1 ... r] >= v
+        int i = l+1, j= r;
+        while (true) {
+            while (i <= j && arr[i].compareTo(arr[l]) < 0) {
+                i++;
+            }
+
+            while (j >= i && arr[j].compareTo(arr[l]) > 0) {
+                j--;
+            }
+            if(i >= j) {
+                break;
+            }
+            swap(arr, i , j);
+            i++;
+            j--;
+        }
+        swap(arr, l ,j);
+        return j;
+
+
+    }
+
+    private int partitionOld(E[] arr, int l, int r) {
         // arr[ l+1 .. j] < v ; arr[j +1 ..i) >= v
         int p = l + ThreadLocalRandom.current().nextInt(r - l + 1);
         swap(arr, l, p);
@@ -46,17 +74,9 @@ public class QuickSort<E extends Comparable<E>> implements Sortable<E> {
     }
 
     public static void main(String[] args) {
-        int n = 5000000;
-        Integer[] arr = ArrayGenerator.generateRandomArray(n, n);
-        Integer[] arr2 = Arrays.copyOf(arr, arr.length);
+        int n = 100000;
+        Integer[] arr = ArrayGenerator.generateRandomArray(n, 1);
+        SortingHelper.sortTest(new QuickSort<>(), arr);
 
-        SortingHelper.sortTest(new MergeSort<>(), arr);
-        SortingHelper.sortTest(new QuickSort<>(), arr2);
-
-
-        arr = ArrayGenerator.generateOrderedArray(n);
-        arr2 = Arrays.copyOf(arr, arr.length);
-        SortingHelper.sortTest(new MergeSort<>(), arr);
-        SortingHelper.sortTest(new QuickSort<>(), arr2);
     }
 }
