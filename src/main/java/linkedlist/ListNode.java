d27151492f5404ed9b523f8bbdc9668a5f6fc7ed
wangjq <1061669148@qq.com>
2021-11-22 14:14:18 +0800

    归并排序-将 listNode 改为 public

--- src/main/java/linkedlist/ListNode.java
+++ src/main/java/linkedlist/ListNode.java
@@ -1,18 +1,18 @@
 package linkedlist;
 
 public class ListNode {
-    int val;
-    ListNode next;
+    public int val;
+    public ListNode next;
 
-    ListNode() {
+    public ListNode() {
 
     }
 
-    ListNode(int val) {
+    public ListNode(int val) {
         this.val = val;
     }
 
-    ListNode(int val, ListNode next) {
+    public ListNode(int val, ListNode next) {
         this.val = val;
         this.next = next;
     }
