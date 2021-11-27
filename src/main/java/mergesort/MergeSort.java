f862b9328b16e4a07276483ef13155bf19d7972e
wangjq <1061669148@qq.com>
2021-11-27 22:05:04 +0800

    二分查找法-改变定义重写归并排序算法

--- src/main/java/mergesort/MergeSort.java
+++ src/main/java/mergesort/MergeSort.java
@@ -21,36 +21,48 @@ public class MergeSort<E extends Comparable<E>> implements Sortable<E> {
 
     @Override
     public void sort(E[] arr) {
-        sort(arr, 0, arr.length - 1);
+        E[] temp = Arrays.copyOf(arr, arr.length);
+        sort(arr, 0, arr.length, temp );
     }
 
-    private void sort(E[] arr, int l, int r) {
-        if (l >= r) {
+    /**
+     * 对 arr[l,r) 范围进行排序
+     */
+    private void sort(E[] arr, int l, int r, E[] temp) {
+
+        // 因为 r 是开区间，所以 r 比 l 多1时，整个数组只有一个元素
+        if (l >= r - 1) {
             return;
         }
 
         int mid = (l + r) / 2;
-        sort(arr, l, mid);
-        sort(arr, mid + 1, r);
-        merge(arr, l, mid, r);
+        sort(arr, l, mid, temp);
+        sort(arr, mid, r, temp);
+        merge(arr, l, mid, r, temp);
     }
 
-    private void merge(E[] arr, int l, int mid, int r) {
-        E[] temp = Arrays.copyOfRange(arr, l, r + 1);
-        int i = l, j = mid + 1;
+    /**
+     * 合并 arr[l, mid) 和 arr[mid, r)
+     */
+    private void merge(E[] arr, int l, int mid, int r, E[] aux) {
+        System.arraycopy(arr, l, aux, l, r-l);
+
+        int i = l, j = mid;
         // 每次循环为 arr[k] 赋值
-        for (int k = l; k <= r; k++) {
-            if (i > mid) {
-                arr[k] = temp[j - l];
+        for (int k = l; k < r; k++) {
+            // 当 i >= mid, 就不用管左侧数组了
+            if (i >= mid) {
+                arr[k] = aux[j];
                 j++;
-            } else if (j > r) {
-                arr[k] = temp[i - l];
+            } else if (j >= r) {
+                // 当 j >=r， 就不用管右侧数组了
+                arr[k] = aux[i];
                 i++;
-            } else if (temp[i - l].compareTo(temp[j - l]) <= 0) {
-                arr[k] = temp[i - l];
+            }else if(aux[i].compareTo(aux[j] )<= 0){
+                arr[k] = aux[i];
                 i++;
-            } else {
-                arr[k] = temp[j - l];
+            }else {
+                arr[k] = aux[j];
                 j++;
             }
         }
