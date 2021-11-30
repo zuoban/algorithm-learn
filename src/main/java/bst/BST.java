6e42cc2e3e6dc8e79cf36503c5ed69c9a23e124e
wangjq <1061669148@qq.com>
2021-11-30 11:52:52 +0800

    二分搜索树-add方法

---
+++ src/main/java/bst/BST.java
@@ -0,0 +1,64 @@
+package bst;
+
+public class BST<E extends Comparable<E>> {
+    private Node root;
+    private int size;
+
+    public BST() {
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    /**
+     * 向二分搜索树中添加新的元素e
+     *
+     * @param e
+     */
+    public void add(E e) {
+        if (root == null) {
+            root = new Node(e);
+            size++;
+        } else {
+            add(root, e);
+        }
+
+    }
+
+    /**
+     * 向以 node 为根的二分搜索树中插入元素 e，递归算法
+     */
+    private void add(Node node, E e) {
+        if (e.equals(node.e)) {
+            return;
+        } else if (e.compareTo(node.e) < 0 && node.left == null) {
+            node.left = new Node(e);
+            size++;
+            return;
+        } else if (e.compareTo(node.e) > 0 && node.right == null) {
+            node.right = new Node(e);
+            size++;
+            return;
+        }
+        if (e.compareTo(node.e) < 0) {
+            add(node.left, e);
+        } else {
+            add(node.right, e);
+        }
+    }
+
+    private class Node {
+        public E e;
+        public Node left, right;
+
+        public Node(E e) {
+            this.e = e;
+        }
+    }
+
+}
