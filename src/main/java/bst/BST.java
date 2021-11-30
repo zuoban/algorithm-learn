441ae7b9796161fd27931159f7c513e336a84642
wangjq <1061669148@qq.com>
2021-11-30 13:31:42 +0800

    二分搜索树-层序遍历

--- src/main/java/bst/BST.java
+++ src/main/java/bst/BST.java
@@ -1,5 +1,7 @@
 package bst;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 public class BST<E extends Comparable<E>> {
@@ -138,6 +140,24 @@ public class BST<E extends Comparable<E>> {
             }
             if (cur.left != null) {
                 stack.push(cur.left);
+            }
+        }
+    }
+
+    /**
+     * 层序遍历
+     */
+    public void levelOrder() {
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty()) {
+            Node cur = q.remove();
+            System.out.println(cur.e);
+            if(cur.left != null) {
+                q.add(cur.left);
+            }
+            if(cur.right != null) {
+                q.add(cur.right);
             }
         }
     }
--- src/main/java/bst/Main.java
+++ src/main/java/bst/Main.java
@@ -3,16 +3,21 @@ package bst;
 public class Main {
     public static void main(String[] args) {
         BST<Integer> bst = new BST<>();
-        int [] nums = {5, 3, 6, 8, 4,2};
+        int[] nums = {5, 3, 6, 8, 4, 2};
         for (int num : nums) {
             bst.add(num);
         }
-
-        bst.preOrderNR();
-        bst.preOrder();
         System.out.println(bst);
 
+        System.out.println("preOrderNR");
+        bst.preOrderNR();
+        System.out.println("preOrder");
+        bst.preOrder();
+
+        System.out.println("inOrder");
         bst.inOrder();
 
+        System.out.println("levelOrder");
+        bst.levelOrder();
     }
 }
