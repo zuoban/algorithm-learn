791b7e25f805aebacb3cb0913782a327a9f5c4a0
wangjq <1061669148@qq.com>
2021-11-30 12:17:42 +0800

    二分搜索树-添加操作，非递归实现

--- src/main/java/bst/BST.java
+++ src/main/java/bst/BST.java
@@ -16,6 +16,42 @@ public class BST<E extends Comparable<E>> {
     }
 
     /**
+     * 向二分搜索树中添加新元素，非递归写法
+     *
+     * @param e
+     */
+    public void add2(E e) {
+        if (root == null) {
+            root = new Node(e);
+            size++;
+            return;
+        }
+        // 用 p 来跟踪待插入的上一个节点, p 的作用相当于链表插入节点时，pre 的作用
+        Node p = root;
+        while (p != null) {
+            // 如果待插入的值小于当前 p 节点的值，说明新插入的值要放在p的左子树
+            if (e.compareTo(p.e) < 0) {
+                // 如果 p 的左子树为空，则在 p.left 上放入新的节点
+                if (p.left == null) {
+                    p.left = new Node(e);
+                    size++;
+                    return;
+                }
+                p = p.left;
+            } else if (e.compareTo(p.e) > 0) {
+                if (p.right == null) {
+                    p.right = new Node(e);
+                    size++;
+                    return;
+                }
+                p = p.right;
+            } else {
+                return;
+            }
+        }
+    }
+
+    /**
      * 向二分搜索树中添加新的元素e
      *
      * @param e
