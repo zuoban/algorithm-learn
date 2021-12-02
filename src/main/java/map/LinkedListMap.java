9ec30c1c0179b449130b7f2b4532767a4a774338
wangjq <1061669148@qq.com>
2021-12-03 07:33:11 +0800

    集合和映射- LinkedListMap

---
+++ src/main/java/map/LinkedListMap.java
@@ -0,0 +1,104 @@
+package map;
+
+public class LinkedListMap<K, V> implements Map<K, V> {
+    private Node dummyHead;
+    private int size;
+
+    @Override
+    public void add(K key, V value) {
+        Node node = getNode(key);
+        if (node != null) {
+            node.value = value;
+        } else {
+            dummyHead.next = new Node(key, value, dummyHead.next);
+            size++;
+        }
+    }
+
+    @Override
+    public V remove(K key) {
+        Node prev = dummyHead;
+        while (prev.next != null) {
+            if (prev.next.key.equals(key)) {
+                break;
+            }
+            prev = prev.next;
+        }
+
+        if (prev.next != null) {
+            Node delNode = prev.next;
+            prev.next = delNode.next;
+            delNode.next = null;
+            size--;
+            return delNode.value;
+        }
+
+        return null;
+    }
+
+    @Override
+    public boolean contains(K key) {
+        return getNode(key) != null;
+    }
+
+    @Override
+    public V get(K key) {
+        Node node = getNode(key);
+        return node == null ? null : node.value;
+    }
+
+    @Override
+    public void set(K key, V newValue) {
+        Node node = getNode(key);
+        if (node == null) {
+            throw new IllegalArgumentException(key + " doesn't exist");
+        }
+
+        node.value = newValue;
+    }
+
+    @Override
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return getSize() == 0;
+    }
+
+    private Node getNode(K key) {
+        Node cur = dummyHead.next;
+        while (cur != null) {
+            if (cur.key.equals(key)) {
+                return cur;
+            }
+            cur = cur.next;
+        }
+        return null;
+    }
+
+    private class Node {
+        public K key;
+        public V value;
+        public Node next;
+
+        public Node(K key, V value, Node next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+
+        public Node(K key) {
+            this(key, null, null);
+        }
+
+        public Node() {
+        }
+
+        @Override
+        public String toString() {
+            return key + ":" + value;
+        }
+    }
+}
