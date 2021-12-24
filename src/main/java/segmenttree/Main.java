66cc1b30237267525b7e5f05070921c79f72be59
zuoban <1061669148@qq.com>
2021-12-24 20:54:30 +0800

    线段树-查询

--- src/main/java/segmenttree/Main.java
+++ src/main/java/segmenttree/Main.java
@@ -5,5 +5,10 @@ public class Main {
         Integer[] nums = {-2, 0, 3, -5, 2, -1};
         SegmentTree<Integer> segmentTree = new SegmentTree<>(nums, (a, b) -> a + b);
         System.out.println(segmentTree);
+
+
+        System.out.println(segmentTree.query(0, 2));
+        System.out.println(segmentTree.query(2, 5));
+        System.out.println(segmentTree.query(0, 5));
     }
 }
--- src/main/java/segmenttree/SegmentTree.java
+++ src/main/java/segmenttree/SegmentTree.java
@@ -59,4 +59,44 @@ public class SegmentTree<E> {
     public String toString() {
         return Arrays.toString(tree);
     }
+
+
+    /**
+     * 返回值 [queryL, queryR] 的值
+     * @param queryL
+     * @return
+     */
+    public E query(int queryL,int queryR) {
+        checkIndex(queryL);
+        checkIndex(queryR);
+        return query(0, 0, data.length - 1, queryL, queryR);
+    }
+
+    /**
+     * 在以 treeIndex 为根的线段树中，[l...r]的范围里，搜索区间 [queryL, queryR]
+     */
+    private E query(int treeIndex, int l , int r, int queryL, int queryR){
+        if(l == queryL && r == queryR) {
+            return tree[treeIndex];
+        }
+        int mid = l + (r-l)/2;
+        int leftTreeIndex = leftChild(treeIndex);
+        int rightTreeIndex = rightChild(treeIndex);
+
+        if(queryL >= mid + 1) {
+            return query(rightTreeIndex, mid+1, r, queryL, queryR);
+        }else if(queryR <= mid) {
+            return query(leftTreeIndex, l, mid, queryL, queryR);
+        }
+
+        E leftResult = query(leftTreeIndex, l, mid, queryL, mid);
+        E rightResult = query(rightTreeIndex, mid+1, r, mid+1, queryR);
+        return merger.merge(leftResult, rightResult);
+    }
+
+    private void checkIndex(int index) {
+        if(index <0 || index >= data.length) {
+            throw new IllegalArgumentException("index out of range");
+        }
+    }
 }
