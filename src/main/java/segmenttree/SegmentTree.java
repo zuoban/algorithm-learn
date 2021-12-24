aec6389180cc977ea340b6c2ba40f032e7033540
zuoban <1061669148@qq.com>
2021-12-24 21:52:53 +0800

    线段树-更新

--- src/main/java/segmenttree/SegmentTree.java
+++ src/main/java/segmenttree/SegmentTree.java
@@ -63,10 +63,11 @@ public class SegmentTree<E> {
 
     /**
      * 返回值 [queryL, queryR] 的值
+     *
      * @param queryL
      * @return
      */
-    public E query(int queryL,int queryR) {
+    public E query(int queryL, int queryR) {
         checkIndex(queryL);
         checkIndex(queryR);
         return query(0, 0, data.length - 1, queryL, queryR);
@@ -75,28 +76,51 @@ public class SegmentTree<E> {
     /**
      * 在以 treeIndex 为根的线段树中，[l...r]的范围里，搜索区间 [queryL, queryR]
      */
-    private E query(int treeIndex, int l , int r, int queryL, int queryR){
-        if(l == queryL && r == queryR) {
+    private E query(int treeIndex, int l, int r, int queryL, int queryR) {
+        if (l == queryL && r == queryR) {
             return tree[treeIndex];
         }
-        int mid = l + (r-l)/2;
+        int mid = l + (r - l) / 2;
         int leftTreeIndex = leftChild(treeIndex);
         int rightTreeIndex = rightChild(treeIndex);
 
-        if(queryL >= mid + 1) {
-            return query(rightTreeIndex, mid+1, r, queryL, queryR);
-        }else if(queryR <= mid) {
+        if (queryL >= mid + 1) {
+            return query(rightTreeIndex, mid + 1, r, queryL, queryR);
+        } else if (queryR <= mid) {
             return query(leftTreeIndex, l, mid, queryL, queryR);
         }
 
         E leftResult = query(leftTreeIndex, l, mid, queryL, mid);
-        E rightResult = query(rightTreeIndex, mid+1, r, mid+1, queryR);
+        E rightResult = query(rightTreeIndex, mid + 1, r, mid + 1, queryR);
         return merger.merge(leftResult, rightResult);
     }
 
     private void checkIndex(int index) {
-        if(index <0 || index >= data.length) {
+        if (index < 0 || index >= data.length) {
             throw new IllegalArgumentException("index out of range");
         }
+    }
+
+    public void set(int index, E e) {
+        checkIndex(index);
+        data[index] = e;
+        set(0, 0, data.length - 1, index, e);
+    }
+
+    private void set(int treeIndex, int l, int r, int index, E e) {
+        if (l == r) {
+            tree[treeIndex] = e;
+        }
+        int mid = l + (r - l) / 2;
+        int leftTreeIndex = leftChild(treeIndex);
+        int rightTreeIndex = rightChild(treeIndex);
+
+        if (index >= mid + 1) {
+            set(rightTreeIndex, mid + 1, r, index, e);
+        } else {
+            set(leftTreeIndex, l, mid, index, e);
+        }
+
+        tree[treeIndex] = merger.merge(tree[leftTreeIndex], tree[rightTreeIndex]);
     }
 }
