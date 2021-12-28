b6e71e03d3f82444ffc616f7394f4e94d0965fef
zuoban <1061669148@qq.com>
2021-12-28 17:35:03 +0800

    并查集-版本3

---
+++ src/main/java/unionfind/Main.java
@@ -0,0 +1,39 @@
+package unionfind;
+
+import java.util.Random;
+
+public class Main {
+
+    private static double testUF(UF uf, int m) {
+        int size = uf.getSize();
+        Random random = new Random();
+        long startTime = System.nanoTime();
+
+        for (int i = 0; i < m; i++) {
+            int a = random.nextInt(size);
+            int b = random.nextInt(size);
+            uf.unionElements(a, b);
+        }
+        for (int i = 0; i < m; i++) {
+            int a = random.nextInt(size);
+            int b = random.nextInt(size);
+            uf.isConnected(a, b);
+        }
+
+        long endTime = System.nanoTime();
+        return (endTime - startTime) / 1_000_000_000.0;
+    }
+
+    public static void main(String[] args) {
+        int size = 100000;
+        int m = 100000;
+        UF uf1 = new UnionFind1(size);
+        System.out.println("UnionFind1: " + testUF(uf1, m) + " s");
+
+        UF uf2 = new UnionFind2(size);
+        System.out.println("UnionFind2: " + testUF(uf2, m) + " s");
+
+        UF uf3 = new UnionFind3(size);
+        System.out.println("UnionFind3: " + testUF(uf3, m) + " s");
+    }
+}
