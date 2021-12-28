41b1f33278a80150e1310047ea155c43ca6ea562
zuoban <1061669148@qq.com>
2021-12-28 17:45:18 +0800

    并查集-版本4

--- src/main/java/unionfind/Main.java
+++ src/main/java/unionfind/Main.java
@@ -25,15 +25,18 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        int size = 100000;
-        int m = 100000;
-        UF uf1 = new UnionFind1(size);
-        System.out.println("UnionFind1: " + testUF(uf1, m) + " s");
-
-        UF uf2 = new UnionFind2(size);
-        System.out.println("UnionFind2: " + testUF(uf2, m) + " s");
+        int size = 10000000;
+        int m = 10000000;
+//        UF uf1 = new UnionFind1(size);
+//        System.out.println("UnionFind1: " + testUF(uf1, m) + " s");
+//
+//        UF uf2 = new UnionFind2(size);
+//        System.out.println("UnionFind2: " + testUF(uf2, m) + " s");
 
         UF uf3 = new UnionFind3(size);
         System.out.println("UnionFind3: " + testUF(uf3, m) + " s");
+
+        UF uf4 = new UnionFind4(size);
+        System.out.println("UnionFind4: " + testUF(uf4, m) + " s");
     }
 }
