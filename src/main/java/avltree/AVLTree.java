81092fb8d59b74915af6eab6de9fe5c81ef6d9c6
zuoban <1061669148@qq.com>
2021-12-29 08:00:36 +0800

    AVL-删除元素

--- src/main/java/avltree/AVLTree.java
+++ src/main/java/avltree/AVLTree.java
@@ -32,42 +32,77 @@ public class AVLTree<K extends Comparable<K>, V> implements Map<K, V> {
 
     @Override
     public V remove(K key) {
-        return null;
+        Node removeNode = remove(root, key);
+        return removeNode == null ? null : removeNode.value;
     }
 
     private Node remove(Node node, K key) {
         if (node == null) {
             return null;
         }
+        Node retNode;
         if (key.compareTo(node.key) < 0) {
             node.left = remove(node.left, key);
+            retNode = node;
         } else if (key.compareTo(node.key) > 0) {
             node.right = remove(node.right, key);
+            retNode = node;
         } else {
             // e == node.e
             if (node.left == null) {
                 Node rightNode = node.right;
                 node.right = null;
                 size--;
-                return rightNode;
-            }
-            if (node.right == null) {
+                retNode = rightNode;
+            } else if (node.right == null) {
                 Node leftNode = node.left;
                 node.left = null;
                 size--;
-                return leftNode;
+                retNode = leftNode;
+            } else {
+                // 待删除节点左右子树均不为空的情况
+                // 找到比待删除节点大的最小节点，即待删除节点右子树的最小节点
+                // 用这个节点顶替删除节点的位置
+                Node successor = minimum(node.right);
+                successor.right = remove(node.right, successor.key);
+                successor.left = node.left;
+                node.left = node.right = null;
+                retNode = successor;
             }
-
-            // 待删除节点左右子树均不为空的情况
-            // 找到比待删除节点大的最小节点，即待删除节点右子树的最小节点
-            // 用这个节点顶替删除节点的位置
-            Node successor = minimum(node.right);
-            successor.right = removeMin(node.right);
-            successor.left = node.left;
-            node.left = node.right = null;
-            return successor;
         }
-        return node;
+        if (retNode == null) {
+            return null;
+        }
+
+        // 更新 retNode 的高度
+        retNode.height = Math.max(getHeight(retNode.left), getHeight(retNode.right)) + 1;
+
+        // 计算平衡因子
+        int balanceFactor = getBalanceFactor(retNode);
+
+        // 平衡维护
+        // LL
+        if (balanceFactor > 1 && getBalanceFactor(retNode.left) >= 0) {
+            return rightRotate(retNode);
+        }
+
+        // RR
+        if (balanceFactor < -1 && getBalanceFactor(retNode.right) <= 0) {
+            return leftRotate(retNode);
+        }
+
+        // LR
+        if (balanceFactor > 1 && getBalanceFactor(retNode.left) < 0) {
+            retNode.left = leftRotate(retNode.left);
+            return rightRotate(retNode);
+        }
+        // RL
+        if (balanceFactor < -1 && getBalanceFactor(retNode.right) > 0) {
+            retNode.right = rightRotate(retNode.right);
+            return leftRotate(retNode);
+        }
+
+        return retNode;
     }
 
     @Override
